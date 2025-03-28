491e3fcec4e0595696cc321c645af9f096185c98
User <gowthami.r0212@gmail.com>
2025-03-28 12:27:03 +0530

    Added

--- portfolio/src/Components/About.jsx
+++
@@ -1,9 +0,0 @@
-function About() {
-    return (
-        <section>
-            <h2>About Me</h2>
-            <p> i'm apprenticeship</p>
-        </section>
-    )
-}
-export default About
--- portfolio/src/Components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p> 2025 i.m going to get a IT job</p>
-        </footer>
-    )
-}
-export default Footer;
