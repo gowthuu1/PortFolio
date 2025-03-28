b04fb53f52f9e27b514988061d8e9b31db289e9f
User <gowthami.r0212@gmail.com>
2025-03-28 12:23:17 +0530

    Added

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import About from './components/About.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Footer />
+      <Header/>
+
+      <About />
+        <Projects />
+        <Contact/>
+
+
+    </>
+  )
+}
+
+export default App
